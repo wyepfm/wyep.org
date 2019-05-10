24b0743ad2ed14d37f8cba634066e6d3b05be39b
unknown <antonio@wyep.org>
2019-05-10 10:40:48 -0400

    Updates

---
+++ component---src-pages-page-2-js-62402b6774f24c11ec03.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{144:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(152),c=a(162),r=a(160);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-62402b6774f24c11ec03.js.map
